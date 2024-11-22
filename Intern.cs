d624675e03fe837e562bbd65785c34ea6a2e64eb
MartinFortuna <martinperricault@gmail.com>
2024-11-22 19:39:32 +0000

    Adding the Intern class

---
+++ Intern.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectManagementSystem;
+
+// Intern inherits from Employee class
+public class Intern : Employee
+{
+    //Using employee class constructor
+    public Intern(string name) : base(name, "Intern") { }
+
+}
