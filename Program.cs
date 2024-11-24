a9de28ecf44a9c829e244d4ea32108167069bff7
Conor McFeely <cwmcfeely@gmail.com>
2024-11-24 19:18:52 +0000

    Updating program for testing

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Developer dev = new Developer("D001", "Conor McFeely","C#");
+        Developer dev = new Developer(123, "Conor", "McFeely","C#");
         
         dev.ExecuteRole();  // Output: Alice is writing code in C#.
         dev.GenerateReport();  // Output: Alice is generating a developer-specific report.
