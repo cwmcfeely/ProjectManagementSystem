79dde1c926473cdd8fc3a5eea5940e0f5d346991
jothikaaravindhan@gmail.com <jothikaaravindhan@gmail.com>
2024-12-05 21:49:55 +0530

    Fixed a minor compilation issue in Program.cs related to an internal class.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,37 @@
-﻿class Program
+﻿namespace ProjectManagementSystem
 {
-    static void Main(string[] args)
+    class Program
     {
-        Developer dev = new Developer(123, "Conor", "McFeely", "C#");
+        static void Main(string[] args)
+        {
+            Developer dev = new Developer(123, "Conor", "McFeely", "C#");
 
-        dev.ExecuteRole();  // Output: Conor is writing code in C#.
-        dev.GenerateReport();  // Output: Conor is generating a developer-specific report.
-
-
-        Manager manager = new Manager(7689, "Zouboulia", "Fanuda");
-
-        manager.ExecuteRole();
-
-        Intern intern = new Intern(8936, "Martin", "Fortuna")
+            dev.ExecuteRole();  // Output: Conor is writing code in C#.
+            dev.GenerateReport();  // Output: Conor is generating a developer-specific report.
 
 
-        intern.ExecuteRole();
+            Manager manager = new Manager(7689, "Zouboulia", "Fanuda");
 
-        Task firstTask = new Task(1234, "Fix Bug", "High");
-        Task secondTask = new Task(1235, "Learn Basic C#", "Medium");
-        Task thirdTask = new Task(1245, "Learn C# Basics", "High");
+            manager.ExecuteRole();
 
-        manager.AssignTask(dev, firstTask); //assign task to dev
-        manager.UpdateStatus();//update task status as a manager
-        manager.GenerateReport();
+            Intern intern = new Intern(8936, "Martin", "Fortuna");
 
 
-        intern.StartTask();
-        intern.CompleteTask()
-        intern.GenerateReport();
+            intern.ExecuteRole();
+
+            Task firstTask = new Task(1234, "Fix Bug", "High");
+            Task secondTask = new Task(1235, "Learn Basic C#", "Medium");
+            Task thirdTask = new Task(1245, "Learn C# Basics", "High");
+
+            manager.AssignTask(dev, firstTask); //assign task to dev
+            manager.UpdateStatus();//update task status as a manager
+            manager.GenerateReport();
+
+
+            intern.StartTask(firstTask);
+            intern.CompleteTask(firstTask);
+            intern.GenerateReport();
+
+        }
     }
 }
